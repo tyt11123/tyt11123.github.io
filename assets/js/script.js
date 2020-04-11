0e23d5c3a1622cabb8af9b576ef03286e8c70740
Kam Yiu Cheung <tyt11123@yahoo.com.hk>
2020-04-11 18:19:41 +0800

    Rewrite in Bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
-var elem = document.querySelector('.grid');
-var imgLoad = imagesLoaded(elem);
+// All animations will take exactly 900ms
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 900
+});
+
+let last_known_scroll_position = 0;
+let elem = document.getElementsByClassName('navbar')[0];
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+  if (last_known_scroll_position > 100) {
+    elem.classList.remove('bg-secondary');
+  } else if (last_known_scroll_position <= 99) {
+    elem.classList.add('bg-secondary');
+  }
+});
+
+var element = document.querySelector('.grid');
+var imgLoad = imagesLoaded(element);
 function onAlways(instance) {
-  var msnry = new Masonry(elem, {
+  var msnry = new Masonry(element, {
     // options
     itemSelector: '.gallery',
     columnWidth: 60
