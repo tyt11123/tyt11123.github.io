fb4f1f4b1b53d09ed0a0ad5f8c22ec8dd24edef9
Kam Yiu Cheung <tyt11123@yahoo.com.hk>
2020-04-17 10:01:49 +0800

    For presentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,19 +16,14 @@ window.addEventListener('scroll', function(e) {
 });
 
 var element = document.querySelector('.grid');
-// var imgLoad = imagesLoaded(element);
-// function onAlways(instance) {
-//   var msnry = new Masonry(element, {
-//     // options
-//     itemSelector: '.gallery',
-//     columnWidth: 60
-//   });
-// }
-// // bind with .on()
-// imgLoad.on('always', onAlways);
+var imgLoad = imagesLoaded(element);
+function onAlways(instance) {
+  var msnry = new Masonry(element, {
+    // options
+    itemSelector: '.gallery',
+    columnWidth: 60
+  });
+}
+// bind with .on()
+imgLoad.on('always', onAlways);
 
-var msnry = new Masonry(element, {
-  // options
-  itemSelector: '.gallery',
-  columnWidth: 60
-});
